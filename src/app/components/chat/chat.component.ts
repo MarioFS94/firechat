9faf32fb3d9bdcfbb78695a76c8ae20862a9c111
mario <mariofernandes1@gmail.com>
2020-03-09 17:31:15 +0100

    Creando tipo de dato Mensaje

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -15,16 +15,13 @@ export class ChatComponent {
   constructor(public chatSrv: ChatService) {
       this.chatSrv.cargarMensajes()
         .subscribe();
-      /*(mensajes:any[])=>{
-          console.log(mensajes);
-        }//poner esto dentro del subcribe para que uestre los mensajes en el template serviria pero seria poco eficiente*/
    }
 
   sendMessage() {
     console.log(this.mensaje);
-    this.msg = this.mensaje
+    /* this.msg = this.mensaje
     this.mensaje = ""; 
-    /* Esto es para pasar la variable del input de enviar mensaje al input del mensaje mostrado con @input desde el componente ts*/
+    Esto es para pasar la variable del input de enviar mensaje al input del mensaje mostrado con @input desde el componente ts*/
   }
 
 }
